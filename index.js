7491c78f282373c6ec17c6aeb7da67a0055e35cc
Vladimir Kudinov <frux@yandex-team.ru>
2015-09-23 19:22:13 +0500

    CSSComb relative path

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = {
     'pre-commit': function(next){
         var stagedFiles = this.stagedFilesSync('ACM'),
-            csscomb = '.githooks/node_modules/csscomb/bin/csscomb',
+            csscomb = __dirname__ '.githooks/node_modules//csscomb/bin/csscomb',
             cssTest = /\.css$/;
 
         stagedFiles.forEach((function(file){
             if(cssTest.test(file)){
                 console.log('CSSComb: ' + file);
-                this.execSync(csscomb, [file]);
+                this.execSync(csscomb + ' ' + file);
                 this.execSync('git', ['add', file]);
             }
         }).bind(this));
