a0d49b08a0211dfbc6842187ae592fefbfb65e2a
Vladimir Kudinov <frux@yandex-team.ru>
2015-09-24 16:56:50 +0500

    Async

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ module.exports = {
     'pre-commit': function(next){
         var stagedFiles = this.stagedFilesSync('ACM'),
             csscomb = __dirname  + '/node_modules/csscomb/bin/csscomb',
-            cssTest = /\.css$/;
+            cssTest = /\.css$/,
+            Uppercode = this;
 
-        stagedFiles.forEach((function(file){
+        stagedFiles.forEach(function(file){
             if(cssTest.test(file)){
                 console.log('CSSComb: ' + file);
-                this.execSync(csscomb + ' ' + file);
-                this.execSync('git', ['add', file]);
+                Uppercode.exec(csscomb + ' ' + file, function(err, data){
+                    if(!err){
+                        Uppercode.exec('git', ['add', file]);
+                    }
+                });
             }
-        }).bind(this));
+        });
 
         next();
     }
